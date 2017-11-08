0cebeb352b505b76b11474700a9ef90cc0a19bb2
phong phu <ppphu1302@gmail.com>
2017-11-08 22:14:24 +0700

    Deleted Project_ReadMe.html

--- KimShop.Model/Abstract/Auditable.cs
+++ KimShop.Model/Abstract/Auditable.cs
@@ -6,15 +6,21 @@ namespace KimShop.Model.Abstract
     public class Auditable : IAuditable
     {
         public DateTime? CreatedDate { get; set; }
+
         [MaxLength(256)]
         public string CreatedBy { get; set; }
+
         public DateTime? UpdatedDate { get; set; }
+
         [MaxLength(256)]
         public string UpdatedBy { get; set; }
+
         [MaxLength(256)]
         public string MetaDescription { get; set; }
+
         [MaxLength(256)]
         public string MetaKeyword { get; set; }
+
         public bool Status { get; set; }
     }
 }
