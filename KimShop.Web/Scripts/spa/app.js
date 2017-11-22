acb03a1b40c6a81be0a4cea6c9d9e621a287c923
phong phu <ppphu1302@gmail.com>
2017-11-22 15:04:52 +0700

    Service in angularjs.

--- KimShop.Web/Scripts/spa/app.js
+++ KimShop.Web/Scripts/spa/app.js
@@ -3,23 +3,24 @@
 var myApp = angular.module("myModule", []);
 
 myApp.controller("schoolController", schoolController);
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
+myApp.service("Validator", Validator);
+schoolController.$inject = ['$scope', 'Validator'];
 
-myController.$inject = ['$scope'];
-
-// Declare
-function schoolController($scope)
-{
-    $scope.message = "Anountment from school.!";
+function schoolController($scope, Validator) {
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber($scope.num);
+    }
+    $scope.num = 1;
 }
 
-// Declare
-function studentController($scope) {
-    //$scope.message = "This is my message from student controller!";
-}
-
-// Declare
-function teacherController($scope) {
-    //$scope.message = "This is my message from teacher controller!";
+function Validator($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0)
+            return 'This is even';
+        else
+            return 'This is odd';
+    }
 }
