ab7d0c14460db37d59972fb679ec51f7b9f9a011
phong phu <ppphu1302@gmail.com>
2017-11-22 13:43:14 +0700

    What is scope and roorScope?

---
+++ KimShop.Web/Scripts/spa/app.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("schoolController", schoolController);
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
+
+myController.$inject = ['$scope'];
+
+// Declare
+function schoolController($scope)
+{
+    $scope.message = "Anountment from school.!";
+}
+
+// Declare
+function studentController($scope) {
+    //$scope.message = "This is my message from student controller!";
+}
+
+// Declare
+function teacherController($scope) {
+    //$scope.message = "This is my message from teacher controller!";
+}
